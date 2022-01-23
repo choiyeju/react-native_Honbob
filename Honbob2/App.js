4bd8bd50fafa36f94f8af1107f098e89ecbf6e1c
choiyeju <yeju1019@gmail.com>
2022-01-23 15:09:31 +0900

    .
    .

---
+++ Honbob2/App.js
@@ -0,0 +1,51 @@
+import App from './src/App';
+export default App;
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  *
+//  * @format
+//  * @flow strict-local
+//  */
+
+// import React, {useState, useEffect} from 'react';
+// import type {Node} from 'react';
+// import {
+//   SafeAreaView,
+//   ScrollView,
+//   StatusBar,
+//   StyleSheet,
+//   Text,
+//   useColorScheme,
+//   View,
+// } from 'react-native';
+
+// import {
+//   Colors,
+//   DebugInstructions,
+//   Header,
+//   LearnMoreLinks,
+//   ReloadInstructions,
+// } from 'react-native/Libraries/NewAppScreen';
+
+// const App = () => {
+
+//   return (
+//     <View style = {styles.container}>
+//       <Text>Hello World</Text>
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container : {
+//     flex : 1,
+//     justifyContent : 'center',
+//     alignItems : 'center',
+//   },
+//   hello : {
+//     color : 'blue'
+//   }
+// });
+
+// export default App;
